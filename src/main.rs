1519a50f84fb70878e46056a060c5cc22e156f00
felipesuri <felipe@felipesuri.com>
2023-02-10 19:41:11 -0300

    feat: :wrench: Add random generator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate rand;
+
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+
 fn main() {
-    println!("Hello, world!");
+    let random_string: String = thread_rng().sample_iter(Alphanumeric).take(32).collect();
+
+    println!("{}", random_string);
 }
